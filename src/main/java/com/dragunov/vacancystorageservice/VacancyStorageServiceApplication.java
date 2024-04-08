6410cc1270bd8cc2799cab361a29a4f010e6d272
Vladimir <mail.dragunov@gmail.com>
2024-04-08 20:56:44 +0300

    first commit

---
+++ src/main/java/com/dragunov/vacancystorageservice/VacancyStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dragunov.vacancystorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragunov/vacancystorageservice/VacancyStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragunov.vacancystorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
