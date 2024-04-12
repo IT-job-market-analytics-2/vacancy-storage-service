d2a926e238a8f875de247cc91b73680c6954812f
Vladimir <mail.dragunov@gmail.com>
2024-04-12 17:26:22 +0300

    add lombok

--- src/main/java/com/dragunov/vacancystorageservice/model/TestEntity.java
+++ src/main/java/com/dragunov/vacancystorageservice/model/TestEntity.java
@@ -1,10 +1,11 @@
 package com.dragunov.vacancystorageservice.model;
 
+import lombok.Data;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
-
+@Data
 @Document(collection = "entities")
 public class TestEntity {
 
@@ -16,36 +17,4 @@ public class TestEntity {
     @Indexed(name = "ttl_index", expireAfterSeconds = 60)
     private Date createdAt;
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public String toString() {
-        return "TestEntity{" +
-                "number=" + number +
-                ", text='" + text + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
 }
