902b29d394b0d7b773116868447c235c90e830dc
Vladimir <mail.dragunov@gmail.com>
2024-04-21 09:02:11 +0300

    refactor: move set ttl index to MongoDbService

--- src/main/java/com/dragunov/vacancystorageservice/service/MongoDbService.java
+++ src/main/java/com/dragunov/vacancystorageservice/service/MongoDbService.java
@@ -4,24 +4,37 @@ import com.dragunov.vacancystorageservice.model.VacanciesEntity;
 import com.dragunov.vacancystorageservice.repository.VacanciesRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.stereotype.Service;
 import org.springframework.dao.DuplicateKeyException;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
 public class MongoDbService {
 
     private final VacanciesRepository repository;
+    private final MongoTemplate mongoTemplate;
+
+    @Value("${spring.data.mongodb.ttl_index}")
+    private int indexTtl;
 
     @Autowired
-    public MongoDbService(VacanciesRepository repository) {
+    public MongoDbService(VacanciesRepository repository, MongoTemplate mongoTemplate) {
         this.repository = repository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public VacanciesEntity addEntity(VacanciesEntity entity) {
         try {
+            log.info("Set TTL index for VacanciesEntity - {} days", indexTtl);
+            mongoTemplate.indexOps(VacanciesEntity.class).ensureIndex(new Index().expire(indexTtl, TimeUnit.DAYS)
+                    .on("createdAt", Sort.Direction.ASC));
             log.info("Saving {}", entity);
             return repository.save(entity);
         } catch (DuplicateKeyException e) {
--- src/main/java/com/dragunov/vacancystorageservice/service/RabbitListenerService.java
+++ src/main/java/com/dragunov/vacancystorageservice/service/RabbitListenerService.java
@@ -2,37 +2,27 @@ package com.dragunov.vacancystorageservice.service;
 
 import com.dragunov.vacancystorageservice.dto.Vacancies;
 import com.dragunov.vacancystorageservice.model.VacanciesEntity;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
+
 
 
 @Component
 public class RabbitListenerService {
-    private final MongoTemplate mongoTemplate;
     private final ConvertService convertService;
     private final MongoDbService mongoDbService;
 
-    @Value("${spring.data.mongodb.ttl_index}")
-    private int indexTtl;
 
     @Autowired
-    public RabbitListenerService(MongoTemplate mongoTemplate, ConvertService convertService, MongoDbService mongoDbService) {
+    public RabbitListenerService(ConvertService convertService, MongoDbService mongoDbService) {
         this.convertService = convertService;
-        this.mongoTemplate = mongoTemplate;
         this.mongoDbService = mongoDbService;
     }
 
     @RabbitListener(queues = "${spring.rabbitmq.queue.imported-vacancies}")
     public void processImportedVacancy(byte[] message) {
-        mongoTemplate.indexOps(VacanciesEntity.class).ensureIndex(new Index().expire(indexTtl, TimeUnit.DAYS).on("createdAt", Sort.Direction.ASC));
         String messageString = new String(message);
         Vacancies vacanciesDto = convertService.convertMessageToDto(messageString);
         VacanciesEntity vacanciesEntity = convertService.convertDtoToEntity(vacanciesDto);
