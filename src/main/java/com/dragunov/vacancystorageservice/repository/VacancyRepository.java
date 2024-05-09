e5989ab3ea6ea951b3bdced0c15d7a75e957d5f3
Vladimir <mail.dragunov@gmail.com>
2024-05-09 12:04:48 +0300

    change Repository

--- src/main/java/com/dragunov/vacancystorageservice/repository/VacancyRepository.java
+++ src/main/java/com/dragunov/vacancystorageservice/repository/VacancyRepository.java
@@ -1,9 +1,9 @@
 package com.dragunov.vacancystorageservice.repository;
 
-import com.dragunov.vacancystorageservice.model.VacanciesEntity;
+import com.dragunov.vacancystorageservice.model.VacancyEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VacanciesRepository extends MongoRepository<VacanciesEntity, String> {
+public interface VacancyRepository extends MongoRepository<VacancyEntity, String> {
 }
